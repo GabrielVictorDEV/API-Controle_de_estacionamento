92992db1a40c32f4cb431b536ae0bff004ef58c9
gabriel-victor-home <gv2074300@gmail.com>
2023-03-27 10:08:19 -0300

    hello world

--- src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -2,12 +2,20 @@ package com.api.parkingcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String olaMundo() {
+		return "Olá Mundo!"; 
 	}
 
 }
